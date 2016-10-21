aa3a8977678affd5a79c398eada5f92d12030c53
robertBrem <brem_robert@hotmail.com>
2016-10-21 15:40:27 +0200

    initial commit

---
+++ run.js
@@ -0,0 +1,62 @@
+#!/usr/bin/jjs -fv
+
+var kubernetesName = "hero-command-test";
+var FileWriter = Java.type("java.io.FileWriter");
+
+var deploymentPath = containerName + "-deployment.yml";
+var fw = new FileWriter(deploymentPath);
+fw.write("apiVersion: extensions/v1beta1\n");
+fw.write("kind: Deployment\n");
+fw.write("metadata:\n");
+fw.write("  name: hero-command\n");
+fw.write("spec:\n");
+fw.write("  replicas: 1\n");
+fw.write("  template:\n");
+fw.write("    metadata:\n");
+fw.write("      labels:\n");
+fw.write("        name: hero-command\n");
+fw.write("        version: " + $ENV.VERSION + "\n");
+fw.write("    spec:\n");
+fw.write("      containers:\n");
+fw.write("      - name: hero-command\n");
+fw.write("        image: robertbrem/heros-command:" + $ENV.VERSION + "\n");
+fw.write("        ports:\n");
+fw.write("          - containerPort: 8080\n");
+fw.close();
+
+var startHeroCommand = "kubectl create -f " + deploymentPath;
+$EXEC(startHeroCommand);
+print($OUT);
+print($ERR);
+
+var servicePath = containerName + "-service.yml";
+var fw = new FileWriter(servicePath);
+fw.write("apiVersion: v1\n");
+fw.write("kind: Service\n");
+fw.write("metadata:\n");
+fw.write("  name: hero-command\n");
+fw.write("  labels:\n");
+fw.write("    name: hero-command\n");
+fw.write("spec:\n");
+fw.write("  ports:\n");
+fw.write("  - name: main\n");
+fw.write("    port: 8282\n");
+fw.write("    targetPort: 8080\n");
+fw.write("    nodePort: 30080\n");
+fw.write("  selector:\n");
+fw.write("    name: hero-command\n");
+fw.write("  type: NodePort\n");
+fw.close();
+
+var startService = "kubectl create -f " + servicePath;
+$EXEC(startService);
+print($OUT);
+print($ERR);
+
+var testUrl = "curl --write-out %{http_code} --silent --output /dev/null http://hero-command:8282/hero-command/resources/heros";
+$EXEC(testUrl);
+while ($OUT != "200") {
+    $EXEC("sleep 1");
+    $EXEC(testUrl);
+    print($OUT);
+}
